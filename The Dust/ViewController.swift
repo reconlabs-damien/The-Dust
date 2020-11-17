4bd7c7498b51f6f1f48642387fc6f72c6175380e
Daesiker <eotlr680@naver.com>
2020-11-17 19:45:54 +0900

    Initial Commit

---
+++ The Dust/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Dust
+//
+//  Created by Jun on 2020/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
